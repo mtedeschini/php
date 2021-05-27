787fe79770388d474d1a37b6d95fd6059a446764
mtedeschini <matiastedeschini@gmail.com>
2021-05-27 13:55:59 -0300

    Cambios

--- empleados.php
+++ empleados.php
@@ -1,17 +1,14 @@
 <?php
-
 #Listado de empleados
 $aEmpleados = array();
 $aEmpleados[] = array("dni" => 33300123, "nombre" => "David García", "bruto" => 85000.30);
 $aEmpleados[] = array("dni" => 40874456, "nombre" => "Ana del Valle", "bruto" => 90000);
 $aEmpleados[] = array("dni" => 67567565, "nombre" => "Andrés Perez", "bruto" => 100000);
 $aEmpleados[] = array("dni" => 75744545, "nombre" => "Victoria Luz", "bruto" => 70000);
-
 #Defino función NETO
 function calcularNeto($bruto){
     return $bruto - ($bruto * 0.17);
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -24,38 +21,36 @@ function calcularNeto($bruto){
     <title>Empleados</title>
 </head>
 <body>
-<div class="container">
-    <div class="row">
-        <div class="col-12 text-center mb-3">
-            <h1>Listado de Empleados</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center mb-3 mt-3">
+                <h1>Listado de Empleados</h1>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-12 text-center">
-            <table class="table border table-hover">
-                <tr>
-                    <th>DNI</th>
-                    <th>Nombre </th>
-                    <th> Sueldo Bruto</th> 
-                    <th> Neto Neto </th> 
-                </tr>
-                <?php
-                    for ($contador = 0 ; $contador < count($aEmpleados); $contador++){?>
+        <div class="row">
+            <div class="col-12 text-center">
+                <table class="table border table-hover">
                     <tr>
-                        <td> <?php echo $aEmpleados[$contador]["dni"]; ?> </td>
-                        <td> <?php echo mb_strtoupper($aEmpleados[$contador]["nombre"]); ?> </td>
-                        <td>$ <?php echo number_format($aEmpleados[$contador]["bruto"], 2 , "," , "." ); ?> </td>
-                        <td>$ <?php echo number_format(calcularNeto($aEmpleados[$contador]["bruto"]) , 2 , "," , "." ); ?> </td>
+                        <th>DNI</th>
+                        <th>Nombre </th>
+                        <th> Sueldo Bruto</th> 
+                        <th> Neto Neto <span style="font-size: 10px">(-17%)</span></th> 
                     </tr>
+                    <?php for ($contador = 0 ; $contador < count($aEmpleados); $contador++){?>
+                        <tr>
+                            <td> <?php echo $aEmpleados[$contador]["dni"]; ?> </td>
+                            <td> <?php echo mb_strtoupper($aEmpleados[$contador]["nombre"]); ?> </td>
+                            <td>$ <?php echo number_format($aEmpleados[$contador]["bruto"], 2 , "," , "." ); ?> </td>
+                            <td>$ <?php echo number_format(calcularNeto($aEmpleados[$contador]["bruto"]) , 2 , "," , "." ); ?> </td>
+                        </tr>
                     <?php } ?>
-            </table>
+                </table>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <p>Cantidad de empleados activos: <?php echo count($aEmpleados) ?></p>
-        </div>
-</div>
-    
+        <div class="row">
+            <div class="col-12">
+                <p>Cantidad de empleados activos: <?php echo count($aEmpleados) ?></p>
+            </div>
+    </div> 
 </body>
 </html>
