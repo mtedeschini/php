c292dbda2d07ef41c67f1e57d03e18091edcd97b
mtedeschini <matiastedeschini@gmail.com>
2021-07-14 19:16:21 -0300

    Cambios

--- login/entidades/localidad.php
+++ login/entidades/localidad.php
@@ -5,43 +5,17 @@ class Localidad{
     private $nombre;
     private $cod_postal;
     private $fk_idprovincia;
-}
 
-public function __construct(){
-}
 
-public function __get($atributo){
-    return $this->$atributo;
-}
-
-public function __set($atributo, $valor){
-    $this->$atributo = $valor;
-    return $this;
-}
-
-public function obtenerTodos(){ // Sólo obtener todos porque no hace falta ABM en provincias
-    $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE); //Abro conexión con BBDD
-    $sql = "SELECT
-                idlocalidad,
-                nombre,
-            FROM clientes
-            ORDER BY nombre ASC";
-    if (!$resultado = $mysqli->query($sql)) {
-        printf("Error en query: %s\n", $mysqli->error . " " . $sql);
+    public function __construct(){
     }
 
-    $aResultado = array();
-    if ($resultado) {
-        //Convierte el resultado en un array asociativo
-        while ($fila = $resultado->fetch_assoc()) {
-            $entidad = new Localidad();
-            $entidadAux->idlocalidad = $fila["idlocalidad"];
-            $entidadAux->nombre = $fila["nombre"];
-            $aResultado[] = $entidadAux;
-        }
+    public function __get($atributo){
+        return $this->$atributo;
     }
-    $mysqli->close(); //Cierro conexión con la BBDD
-    return $aResultado;
-}
 
-?>
+    public function __set($atributo, $valor){
+        $this->$atributo = $valor;
+        return $this;
+    }
+}
--- login/entidades/provincia.php
+++ login/entidades/provincia.php
@@ -3,43 +3,43 @@
 class Provincia{
     private $idprovincia;
     private $nombre;
-}
 
-public function __construct(){
-}
 
-public function __get($atributo){
-    return $this->$atributo;
-}
-
-public function __set($atributo, $valor){
-    $this->$atributo = $valor;
-    return $this;
-}
-
-public function obtenerTodos(){ // Sólo obtener todos porque no hace falta ABM en provincias
-    $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE); //Abro conexión con BBDD
-    $sql = "SELECT
-                idprovincia,
-                nombre
-            FROM provincias
-            ORDER BY nombre ASC";
-    if (!$resultado = $mysqli->query($sql)) {
-        printf("Error en query: %s\n", $mysqli->error . " " . $sql);
+    public function __construct(){
     }
 
-    $aResultado = array();
-    if ($resultado) {
-        //Convierte el resultado en un array asociativo
-        while ($fila = $resultado->fetch_assoc()) {
-            $entidad = new Provincia();
-            $entidadAux->idprovincia = $fila["idprovincia"];
-            $entidadAux->nombre = $fila["nombre"];
-            $aResultado[] = $entidadAux;
+    public function __get($atributo){
+        return $this->$atributo;
+    }
+
+    public function __set($atributo, $valor){
+        $this->$atributo = $valor;
+        return $this;
+    }
+
+    public function obtenerTodos(){ // Sólo obtener todos porque no hace falta ABM en provincias
+        $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE); //Abro conexión con BBDD
+        $sql = "SELECT
+                    idprovincia,
+                    nombre
+                FROM provincias
+                ORDER BY nombre ASC";
+        if (!$resultado = $mysqli->query($sql)) {
+            printf("Error en query: %s\n", $mysqli->error . " " . $sql);
         }
-    }
-    $mysqli->close(); //Cierro conexión con la BBDD
-    return $aResultado;
-}
 
+        $aResultado = array();
+        if ($resultado) {
+            //Convierte el resultado en un array asociativo
+            while ($fila = $resultado->fetch_assoc()) {
+                $entidadAux = new Provincia();
+                $entidadAux->idprovincia = $fila["idprovincia"];
+                $entidadAux->nombre = $fila["nombre"];
+                $aResultado[] = $entidadAux;
+            }
+        }
+        $mysqli->close(); //Cierro conexión con la BBDD
+        return $aResultado;
+        }   
+}
 ?>
